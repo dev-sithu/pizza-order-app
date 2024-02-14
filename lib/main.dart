a43224950f9ec06c353c3e991f6fd0d63c77dff6
Sithu <cithukyaw@gmail.com>
2024-02-14 14:17:01 +0630

    Update home page using appBar with action icons

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,49 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
+          leading: GestureDetector(
+            onTap: () => {
+              debugPrint('Leading icon clicked!')
+            },
+            child: const Icon(Icons.menu),
+          ),
+          title: GestureDetector(
+            onTap: () => {
+              debugPrint('Title clicked!')
+            },
+              child: RichText(
+              text: const TextSpan(
+                text: 'Delivery To:',
+                style: TextStyle(fontSize: 16),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '\nHOME',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      height: 1.5,
+                    ),
+                  ),
+                ]
+              ),
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.shopping_bag),
+              onPressed: () {
+                debugPrint('Action icon clicked');
+              },
+            ),
+            const SizedBox(width: 20)
+          ],
+        ),
+        body: const Center(
           child: Text('Hello World!'),
         ),
       ),
