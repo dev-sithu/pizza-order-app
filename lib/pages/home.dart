c720e16ef137ea4c3b403b6f1b3ed239f9e3e7d2
Sithu <cithukyaw@gmail.com>
2024-02-15 12:58:00 +0630

    Update home page with header texts and 2 empty boxes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -19,17 +19,13 @@ class Home extends StatelessWidget {
           onTap: () => {
             debugPrint('Title clicked!')
           },
-            child: RichText(
-            text: TextSpan(
-              text: 'Delivery To:',
-              style: textBody(),
-              children: <TextSpan>[
-                TextSpan(
-                  text: '\nHOME',
-                  style: textPreTitle(),
-                ),
-              ]
-            ),
+          child: RichText(
+          text: TextSpan(
+            text: 'Delivery To:',
+            style: textBody(),
+            children: <TextSpan>[
+              TextSpan(text: '\nHOME', style: textPreTitle()),
+            ]),
           ),
         ),
         actions: [
@@ -42,9 +38,84 @@ class Home extends StatelessWidget {
           const SizedBox(width: 20)
         ],
       ),
-      body: const Center(
-        child: Text('Hello World!'),
-      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Half BG / Hero section
+            Container(
+              width: double.infinity,
+              height: 152,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xffFFA360),
+                    Color(0xffF5313F),
+                  ],
+                )
+              ),
+              // Header
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30, right: 20, bottom: 2),
+                    child: Text('Good Day!', style: textBody(color: Colors.white)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: IntrinsicHeight(
+                      child: Row(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(left: 5, right: 5),
+                            child: VerticalDivider(color: Colors.white, thickness: 2),
+                          ),
+                          RichText(
+                            text: TextSpan(
+                              text: 'What',
+                              style: textHeader1(color: Colors.white),
+                              children: [
+                                TextSpan(text: ' pizza', style: textHeader2()),
+                                TextSpan(text: ' do you\nwant to try today?', style: textHeader1())
+                              ]
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Card / Reorder
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Container(
+                height: 218,
+                transform: Matrix4.translationValues(0.0, -35.0, 0.0),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                )
+              ),
+            ),
+            // Card / Create Pizza
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Container(
+                height: 438,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                )
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
--- lib/utils/font_style.dart
+++ lib/utils/font_style.dart
@@ -1,66 +1,66 @@
 import 'package:flutter/material.dart';
 
-TextStyle textHeader1() {
+TextStyle textHeader1({Color? color}) {
   return const TextStyle(
     fontSize: 25.0,
     fontWeight: FontWeight.normal,
     letterSpacing: -0.3,
-  );
+  ).copyWith(color: color);
 }
 
-TextStyle textHeader2() {
+TextStyle textHeader2({Color? color}) {
   return const TextStyle(
     fontSize: 25.0,
     fontWeight: FontWeight.bold,
     letterSpacing: -0.3,
-  );
+  ).copyWith(color: color);
 }
 
-TextStyle textHeader3() {
+TextStyle textHeader3({Color? color}) {
   return const TextStyle(
     fontSize: 20.0,
     fontWeight: FontWeight.normal,
     letterSpacing: -0.3,
-  );
+  ).copyWith(color: color);
 }
 
-TextStyle textHeader4() {
+TextStyle textHeader4({Color? color}) {
   return const TextStyle(
     fontSize: 20.0,
     fontWeight: FontWeight.bold,
     letterSpacing: -0.3,
-  );
+  ).copyWith(color: color);
 }
 
-TextStyle textPreTitle() {
+TextStyle textPreTitle({Color? color}) {
   return const TextStyle(
     fontSize: 10.0,
     fontWeight: FontWeight.bold,
     letterSpacing: 1,
-  );
+  ).copyWith(color: color);
 }
 
-TextStyle textButtonDefault() {
+TextStyle textButtonDefault({Color? color}) {
   return const TextStyle(
     fontSize: 15.0,
     fontWeight: FontWeight.normal,
     letterSpacing: -0.3,
-  );
+  ).copyWith(color: color);
 }
 
-TextStyle textButtonSelected() {
+TextStyle textButtonSelected({Color? color}) {
   return const TextStyle(
     fontSize: 15.0,
     fontWeight: FontWeight.bold,
     letterSpacing: -0.3,
-  );
+  ).copyWith(color: color);
 }
 
-TextStyle textBody() {
+TextStyle textBody({Color? color}) {
   return const TextStyle(
     fontSize: 14.0,
     fontWeight: FontWeight.normal,
     letterSpacing: 0,
     height: 1.5,
-  );
+  ).copyWith(color: color);
 }
